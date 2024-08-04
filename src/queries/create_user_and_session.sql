f077333c9936b2b84a701399c9459e24eda96ba6
Aaron Geiger <aaron@geigr.dev>
2024-08-05 01:06:19 +0200

    Remove transaction

--- src/queries/create_user_and_session.sql
+++ src/queries/create_user_and_session.sql
@@ -1,10 +1,6 @@
-BEGIN TRANSACTION;
-
 INSERT INTO user (username, password)
 VALUES (?, ?);
 
 INSERT INTO session (user_id)
 SELECT last_insert_rowid() AS user_id
 RETURNING *;
-
-END TRANSACTION;
