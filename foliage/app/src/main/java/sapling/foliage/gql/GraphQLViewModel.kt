8c75807188fc417d0e8dfd6071c99eb784ee6e67
Aaron Geiger <aaron@geigr.dev>
2025-05-25 19:12:38 +0200

    Add settings screen

--- foliage/app/src/main/java/sapling/foliage/gql/GraphQLViewModel.kt
+++ foliage/app/src/main/java/sapling/foliage/gql/GraphQLViewModel.kt
@@ -4,23 +4,27 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Query
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import sapling.foliage.ProductsQuery
 
-class GraphQLViewModel(private val apolloClient: ApolloClient) : ViewModel() {
-    private val _uiState = MutableStateFlow("Loading...")
-    val uiState: StateFlow<String> = _uiState;
 
-    fun fetchData() {
+class GraphQLViewModel<T : Query<D>, D : Query.Data>(private val apolloClient: ApolloClient) :
+    ViewModel() {
+    private val _uiState = MutableStateFlow<D?>(null)
+    val uiState: StateFlow<D?> = _uiState;
+
+    fun fetch(query: T) {
         viewModelScope.launch {
+            ProductsQuery
             try {
-                val response = apolloClient.query(ProductsQuery()).execute()
-                _uiState.value = response.dataOrThrow().products.toString()
+                val response = apolloClient.query(query).execute()
+                val data = response.dataOrThrow()
+                _uiState.value = data;
             } catch (e: Exception) {
                 Log.e("GraphQLViewModel", "Error fetching data", e)
-                _uiState.value = "Error: ${e.message}"
             }
         }
     }
--- foliage/app/src/main/java/sapling/foliage/ui/screens/SettingsScreen.kt
+++ foliage/app/src/main/java/sapling/foliage/ui/screens/SettingsScreen.kt
@@ -1,47 +1,34 @@
 package sapling.foliage.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Adb
-import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import me.zhanghai.compose.preference.ProvidePreferenceLocals
-import me.zhanghai.compose.preference.switchPreference
+import androidx.preference.PreferenceScreen
 import me.zhanghai.compose.preference.textFieldPreference
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
-    Scaffold(
-    ) { padding ->
-        ProvidePreferenceLocals {
-            LazyColumn(modifier = modifier, contentPadding = padding) {
-                switchPreference(
-                    key = "use_debug_server",
-                    defaultValue = false,
-                    title = { Text(text = "Use debug server") },
-                    summary = { Text(text = if (it) "On" else "Off") })
-
-                textFieldPreference(
-                    key = "gql_endpoint",
-                    title = { Text(text = "GQL Endpoint") },
-                    defaultValue = "https://sapling.geigr.dev/gql",
-                    summary = { Text(text = it) },
-                    textToValue = { x -> x })
-
-
-                textFieldPreference(
-                    key = "debug_gql_endpoint",
-                    title = { Text(text = "GQL Debug Endpoint") },
-                    defaultValue = "http://sapling.local:3000/gql",
-                    summary = { Text(text = it) },
-                    textToValue = { x -> x })
-            }
+    Scaffold(modifier) { contentPadding ->
+        LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = contentPadding) {
+            textFieldPreference(
+                key = "home_server_url",
+                defaultValue = "https://sapling.geigr.dev/gql",
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = null
+                    )
+                },
+                title = { Text(text = "Home Server URL") },
+                summary = { Text(it) },
+                textToValue = { it }
+            )
         }
     }
 }
